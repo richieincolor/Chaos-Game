41282e0d7228f2fbb62e5cc30ab3670273debd50
Richie Gomes <richiegomesdev@gmail.com>
2024-04-15 15:53:58 -0700

    working on it

--- code/main.cpp
+++ code/main.cpp
@@ -5,15 +5,36 @@
 
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
-//#include <SFML/Audio.hpp>
+// #include <SFML/Audio.hpp>
 
 using namespace std;
 using namespace sf;
 
-int main(){
+int main()
+{
+    // Window
     RenderWindow window(VideoMode(800, 600), "Testing");
-    cout << "Hello World!" << endl;
     Event ev;
-    
-    
+
+    // Game Loop
+    while (window.isOpen())
+    {
+        while (window.pollEvent(ev))
+        {
+            switch (ev.type)
+            {
+            case Event::Closed:
+                window.close();
+                break;
+            case Event::KeyPressed:
+                if (ev.key.code == Keyboard::Escape)
+                {
+                    window.close();
+                }
+                if (ev.key.code == Keyboard::Up)
+                    cout << "You have pressed the up key\n";
+                break;
+            }
+        }
+    }
 }
