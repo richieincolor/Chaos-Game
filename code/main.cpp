5f3e7ee46e7a330109b2672c667a9ef2e68ebafd
Richie Gomes <richiegomesdev@gmail.com>
2024-04-15 15:34:13 -0700

    first commit

---
+++ code/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+// #include <vector>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+//#include <SFML/Audio.hpp>
+
+using namespace std;
+using namespace sf;
+
+int main(){
+    RenderWindow window(VideoMode(800, 600), "Testing");
+    cout << "Hello World!" << endl;
+    Event ev;
+    
+    
+}
